2d495fc65c37336632ce9f9cc87964174415a24c
Nate Marks <nmarks@imprivata.com>
2020-07-01 07:44:00 -0400

    Strip out replication bucket references. I retained that in a separate branch

--- modules/backend/main.tf
+++ modules/backend/main.tf
@@ -1,22 +1,14 @@
 # Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
-
-  version = "~> 2.24.0"
-}
-
-provider "aws" {
-  region = var.s3_replication_destination_region
-  alias = "repl_region"
-
-  version = "~> 2.24.0"
 }
 
 
+
 # -------------------------------------------------------------------------------------
-# Add policy to require secure transport on the backend bucket
+# Policy document that requires TLS to S3
 # -------------------------------------------------------------------------------------
-data "aws_iam_policy_document" "backend_bucket_secure_transport" {
+data "aws_iam_policy_document" "require_secure_transport" {
   statement {
     sid = "DenyInsecureCommunications"
     actions = [
@@ -29,7 +21,7 @@ data "aws_iam_policy_document" "backend_bucket_secure_transport" {
     }
     resources = [
       "${aws_s3_bucket.backend_bucket.arn}/*",
-      "${aws_s3_bucket.backend_bucket.arn}"
+      aws_s3_bucket.backend_bucket.arn
     ]
     condition {
       test = "Bool"
@@ -39,18 +31,20 @@ data "aws_iam_policy_document" "backend_bucket_secure_transport" {
   }
 }
 
-resource "aws_s3_bucket_policy" "disable_insecure_s3_communications" {
+
+
+# -------------------------------------------------------------------------------------
+# Attach TLS policy to backend_bucket
+# -------------------------------------------------------------------------------------
+resource "aws_s3_bucket_policy" "backend_bucket_secure_comm" {
   bucket = aws_s3_bucket.backend_bucket.id
-  policy = data.aws_iam_policy_document.backend_bucket_secure_transport.json
+  policy = data.aws_iam_policy_document.require_secure_transport.json
 }
 
 
 # -------------------------------------------------------------------------------------
 # Create the s3 backend bucket and access_block
 # -------------------------------------------------------------------------------------
-
-
-
 resource "aws_s3_bucket" "backend_bucket" {
   bucket = "${var.organization_id}${var.aws_account_id}-terragrunt-remote-state"
   acl    = "private"
@@ -64,21 +58,7 @@ resource "aws_s3_bucket" "backend_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "aws/s3"
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-  replication_configuration {
-    count = var.enable_replication_bucket ? 1 : 0
-
-    role = aws_iam_role.replication[0].arn
-
-    rules {
-      id = "Terragrunt-State-Replication"
-      status = "Enabled"
-      destination {
-        bucket = aws_s3_bucket.replication_destination[0].arn
+        sse_algorithm     = "AES256"
       }
     }
   }
@@ -92,8 +72,9 @@ resource "aws_s3_bucket" "backend_bucket" {
 
 }
 
-
-
+# -------------------------------------------------------------------------------------
+# Set the public access block on backend_bucket
+# -------------------------------------------------------------------------------------
 resource "aws_s3_bucket_public_access_block" "tfstate_backend_bucket_public_block" {
   bucket = aws_s3_bucket.backend_bucket.id
 
@@ -104,133 +85,11 @@ resource "aws_s3_bucket_public_access_block" "tfstate_backend_bucket_public_bloc
 }
 
 
-# -------------------------------------------------------------------------------------
-# Create the replication s3 backend bucket and access_block
-# -------------------------------------------------------------------------------------
-
-
-resource "aws_s3_bucket" "replication_destination" {
-  count = var.enable_replication_bucket ? 1 : 0
-
-  bucket = "${var.organization_id}${var.aws_account_id}-terragrunt-repl"
-  acl    = "private"
-  region   = var.s3_replication_destination_region
-  provider = aws.repl_region
-
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "AES256"
-      }
-    }
-  }
-
- tags = merge({
-    Name      = "${var.organization_id}${var.aws_account_id}-terragrunt-repl"
-    terraform = "true"
-    terragrunt = "true"
-  },
- var.custom_tags)
-}
-
-
-resource "aws_s3_bucket_policy" "tls_for_replication_bucket" {
-  count = var.enable_replication_bucket ? 1 : 0
-
-  bucket = aws_s3_bucket.replication_destination[0].id
-  policy = data.aws_iam_policy_document.backend_bucket_secure_transport.json
-}
-
-resource "aws_s3_bucket_public_access_block" "tfstate_repl_public_block" {
-  count = var.enable_replication_bucket ? 1 : 0
-
-  provider = aws.repl_region
-  bucket = aws_s3_bucket.replication_destination[0].id
-
-
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls = true
-  restrict_public_buckets = true
-}
 
 
 # -------------------------------------------------------------------------------------
-# Configure bucket replicaton
+# Create the dynamodb lock table
 # -------------------------------------------------------------------------------------
-
-
-
-resource "aws_iam_role" "replication" {
-  count = var.enable_replication_bucket ? 1 : 0
-
-  name = "s3crr_${var.aws_account_id}-terragrunt-repl"
-  path   = "/service-role/"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "s3.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_policy" "replication" {
-  count = var.enable_replication_bucket ? 1 : 0
-
-  name = "s3crr_${var.aws_account_id}-terragrunt-repl"
-  path   = "/service-role/"
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "s3:Get*",
-                "s3:ListBucket"
-            ],
-            "Effect": "Allow",
-            "Resource": [
-                "arn:aws:s3:::${aws_s3_bucket.backend_bucket.id}",
-                "arn:aws:s3:::${aws_s3_bucket.backend_bucket.id}/*"
-            ]
-        },
-        {
-            "Action": [
-                "s3:ReplicateObject",
-                "s3:ReplicateDelete",
-                "s3:ReplicateTags",
-                "s3:GetObjectVersionTagging"
-            ],
-            "Effect": "Allow",
-            "Resource": "arn:aws:s3:::${aws_s3_bucket.replication_destination[0].id}/*"
-        }
-    ]
-}
-POLICY
-}
-
-
-resource "aws_iam_role_policy_attachment" "replication" {
-  count = var.enable_replication_bucket ? 1 : 0
-
-  role       = aws_iam_role.replication[0].name
-  policy_arn = aws_iam_policy.replication[0].arn
-}
-
-
 resource "aws_dynamodb_table" "terraform_state_lock" {
   name           = var.lock_table_name
   read_capacity  = 5
